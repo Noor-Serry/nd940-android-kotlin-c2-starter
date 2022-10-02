7afb4baf7fcc9bc6ff1112a634f586977e095945
noor serry <noor.serry.94@gmail.com>
2022-10-02 20:49:13 +0200

    "upgrade android studio"

--- starter/app/src/main/java/com/udacity/asteroidradar/data/room/RoomDB.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/data/room/RoomDB.kt
@@ -5,7 +5,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [Asteroid::class, PictureOfDay::class], version = 3)
+@Database(entities = [Asteroid::class, PictureOfDay::class], version = 4)
 abstract class RoomDB : RoomDatabase() {
 
     abstract fun getDAO():DAO
